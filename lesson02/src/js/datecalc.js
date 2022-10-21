02dbc366d088d6e689c30017a3dbd2dddd447e67
AlexShep5 <alxshep25@gmail.com>
2022-10-21 20:35:08 +0300

    lesson02

---
+++ lesson02/src/js/datecalc.js
@@ -0,0 +1,76 @@
+import { DateTime } from "luxon";
+import {Howl} from 'howler';
+import endSound from "../media/audio/sound.mp3";
+
+export function diffDates (firstDate, secondDate) {
+    firstDate = DateTime.fromISO(firstDate);
+    secondDate = DateTime.fromISO(secondDate);103
+103
+    if (firstDate > secondDate) {
+        secondDate = [firstDate, firstDate = secondDate][0];
+    }
+
+    return secondDate.diff(firstDate, ['year', 'months', 'day']).toObject();
+}
+
+export function diffToHtml (diff) {
+    return `
+        <span>
+            ${diff.years ? 'Лет: ' + diff.years : ''}
+            ${diff.months ? 'Месяцев: ' + diff.months : ''}
+            ${diff.days ? 'Дней: ' + diff.days : ''}
+        </span>
+    `
+}
+
+/*
+    Управление таймером
+*/
+
+const timer_01 = {
+    timerId: null,
+    input: null,
+    btnStart: null,
+    btnStop: null,
+    pMessage: null,
+    sound: new Howl({src: [endSound],}),
+
+    start() {
+        if (this.input.value <= 0) {
+            this.error('Уставка таймера должно быть больше нуля');
+        } else {
+            this.pMessage.innerHTML = '';
+            this.timerId = setInterval(() => {
+                this.input.value--;
+                if (this.input.value == 0) {
+                    clearInterval(this.timerId);
+                    this.btnStop.setAttribute('disabled', false);
+                    this.btnStart.removeAttribute('disabled');
+                    this.sound.play();
+                }
+            }, 1000);
+            this.btnStart.setAttribute('disabled', false);
+            this.btnStop.removeAttribute('disabled');
+        }
+    },
+
+    stop() {
+        clearInterval(this.timerId);
+        this.btnStop.setAttribute('disabled', false);
+        this.btnStart.removeAttribute('disabled');
+    },
+
+    error(mes) {
+        this.pMessage.innerHTML = `${mes}`;
+    }
+}
+
+export function getTimer(inputElm, btnStart, btnStop, pMessage) {
+    const timer = {};
+    Object.assign(timer, timer_01);
+    timer.input = inputElm;
+    timer.btnStart = btnStart;
+    timer.btnStop = btnStop;
+    timer.pMessage = pMessage;
+    return timer;
+}
---
+++ lesson02/webpack.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: resolve(__dirname, 'build'),
+        filename: 'main.[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: resolve(__dirname, 'src', 'index.html')}),
+        new MiniCssExtractPlugin({filename: '[name].[contenthash].css'}),
+        // new BundleAnalyzerPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|mp3)$/i,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        port: 4040
+        }
+};
